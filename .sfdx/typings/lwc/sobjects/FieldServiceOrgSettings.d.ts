b4857684ccfadaf27321301cac944f243cade3f7
Nathan <marcnathanielsantos@gmail.com>
2022-04-30 11:14:43 +0800

    Added gallery component to property page

---
+++ .sfdx/typings/lwc/sobjects/FieldServiceOrgSettings.d.ts
@@ -0,0 +1,48 @@
+declare module "@salesforce/schema/FieldServiceOrgSettings.Id" {
+  const Id:any;
+  export default Id;
+}
+declare module "@salesforce/schema/FieldServiceOrgSettings.IsDeleted" {
+  const IsDeleted:boolean;
+  export default IsDeleted;
+}
+declare module "@salesforce/schema/FieldServiceOrgSettings.DeveloperName" {
+  const DeveloperName:string;
+  export default DeveloperName;
+}
+declare module "@salesforce/schema/FieldServiceOrgSettings.Language" {
+  const Language:string;
+  export default Language;
+}
+declare module "@salesforce/schema/FieldServiceOrgSettings.MasterLabel" {
+  const MasterLabel:string;
+  export default MasterLabel;
+}
+declare module "@salesforce/schema/FieldServiceOrgSettings.CreatedDate" {
+  const CreatedDate:any;
+  export default CreatedDate;
+}
+declare module "@salesforce/schema/FieldServiceOrgSettings.CreatedBy" {
+  const CreatedBy:any;
+  export default CreatedBy;
+}
+declare module "@salesforce/schema/FieldServiceOrgSettings.CreatedById" {
+  const CreatedById:any;
+  export default CreatedById;
+}
+declare module "@salesforce/schema/FieldServiceOrgSettings.LastModifiedDate" {
+  const LastModifiedDate:any;
+  export default LastModifiedDate;
+}
+declare module "@salesforce/schema/FieldServiceOrgSettings.LastModifiedBy" {
+  const LastModifiedBy:any;
+  export default LastModifiedBy;
+}
+declare module "@salesforce/schema/FieldServiceOrgSettings.LastModifiedById" {
+  const LastModifiedById:any;
+  export default LastModifiedById;
+}
+declare module "@salesforce/schema/FieldServiceOrgSettings.SystemModstamp" {
+  const SystemModstamp:any;
+  export default SystemModstamp;
+}
